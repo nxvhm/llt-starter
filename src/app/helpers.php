53f86ca94e9e8c11b3110a7c932c283e84d95077
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2025-05-21 21:22:56 +0300

    Autoload helpers

---
+++ src/app/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+if(!function_exists('user')) {
+	function user(): ?\App\Models\User {
+ 		return Illuminate\Support\Facades\Auth::user();
+	}
+}
+
+if(!function_exists('prev_page')) {
+	function prev_page(string $default = '/'): string {
+		return request()->header('Referer') ?? $default;
+	}
+}
+
+if(!function_exists('getRawSql')) {
+	function getRawSql($q) {
+    $addSlashes = str_replace('?', "'?'", $q->toSql());
+    return vsprintf(str_replace('?', '%s', $addSlashes), $q->getBindings());
+	}
+}
+
+if(!function_exists('currentRouteName')) {
+	function currentRouteName(): string {
+		return Illuminate\Support\Facades\Route::currentRouteName();
+	}
+}
