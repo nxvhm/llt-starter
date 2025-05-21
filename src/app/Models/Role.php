07fb5f321d9b28d30ce81fc491626f15ae713f26
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2025-05-21 20:34:57 +0300

    Custom role/permission models

---
+++ src/app/Models/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Models\Role as SpatieRole;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Role extends SpatieRole {
+	use HasFactory;
+}
