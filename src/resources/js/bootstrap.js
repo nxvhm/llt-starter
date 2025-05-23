248923767e681c7624c9d303423c6ea0daf91229
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2025-05-23 15:25:23 +0300

    update build

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -6,7 +6,11 @@ import './../tabler/css/tabler-props.css'
 import './../tabler/css/tabler-vendors.css'
 import './../tabler/css/tabler-themes.css'
 import '@tabler/icons-webfont/dist/tabler-icons.min.css'
+import "toastify-js/src/toastify.css"
 
 import './../tabler/js/tabler.js'
 
+import Toastify from 'toastify-js'
+
+window.toast = Toastify;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
